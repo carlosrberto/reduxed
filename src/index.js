85b85c76b6d63f40e6b64a7287f92f80505c4020
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2018-12-20 23:02:01 -0200

    added tests for handler and create

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-export const sum = (a, b) => a +b;
+export const create = (...handlers) =>
+  (initialState, options = { typePrefix: '' }) => {
+    const handlersByType = handlers.reduce((acc, { name, fn }) => {
+      acc[`${options.typePrefix}${name}`] = fn;
+      return acc;
+    }, {});
+    return ({
+      handlers, handlersByType, initialState, options,
+    });
+  };
+
+export const handler = (name, fn) =>
+  ({ name, fn });
+
+export const getReducer = ({ handlersByType }) =>
+  (initialState, action) =>
+    (initialState ? handlersByType[action.type](initialState, action) : initialState);
+
+export const getTypes = ({ handlers, options: { typePrefix } }) =>
+  handlers.reduce((acc, { name }) => {
+    acc[name] = `${typePrefix}${name}`;
+    return acc;
+  }, {});
+
+export const getActions = ({ handlers, options: { typePrefix } }) =>
+  handlers.reduce((acc, { name }) => {
+    acc[name] = payload => ({
+      type: `${typePrefix}${name}`,
+      payload,
+    });
+    return acc;
+  }, {});
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,53 @@
-import { sum } from './index';
+import { create, handler } from './index';
 
-describe('sum', () => {
-  it('should sum', () => {
-    expect(sum(1, 2)).toEqual(3);
+describe('create', () => {
+  const fn1 = () => {};
+  const fn2 = () => {};
+  const name1 = 'name1';
+  const name2 = 'name2';
+
+  const args = [
+    handler(name1, fn1),
+    handler(name2, fn2),
+  ];
+
+  const initialState = {};
+  const options = {
+    typePrefix: 'app/foo/',
+  };
+
+  const created = create(...args)(initialState, options);
+
+  it('should return an object with `handlers` key', () => {
+    expect(created.handlers).toEqual(args);
+  });
+
+  it('should return an object with `handlersByType` key', () => {
+    expect(created.handlersByType).toEqual({
+      [`${options.typePrefix}${name1}`]: fn1,
+      [`${options.typePrefix}${name2}`]: fn2,
+    });
+  });
+
+  it('should return an object with `initialState` key', () => {
+    expect(created.initialState).toEqual(initialState);
+  });
+
+  it('should return an object with `options` key', () => {
+    expect(created.options).toEqual(options);
+  });
+});
+
+describe('handler', () => {
+  const name = 'foo';
+  const fn = () => {};
+  const handlerObject = handler(name, fn);
+
+  it('should return an object with a `name` key', () => {
+    expect(handlerObject.name).toEqual(name);
+  });
+
+  it('should return an object with a `fn` key', () => {
+    expect(handlerObject.fn).toEqual(fn);
   });
 });
