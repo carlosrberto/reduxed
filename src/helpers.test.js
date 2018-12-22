d40927073498a4873104633ae14bf5ba6519ca5d
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2018-12-22 18:15:45 -0200

    added test to isObject and isFunction

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,4 +1,9 @@
-import { toSnakeCase, getActionType } from './helpers';
+import {
+  toSnakeCase,
+  getActionType,
+  isObject,
+  isFunction,
+} from './helpers';
 
 describe('toSnakeCase', () => {
   it('should convert a camel case string to snake case', () => {
@@ -11,5 +16,37 @@ describe('getActionType', () => {
   it('should return the prefix concatenated with name', () => {
     expect(getActionType('foo/bar', 'fooBarAction'))
       .toEqual('foo/bar/FOO_BAR_ACTION');
+  });
+});
+
+describe('isFunction', () => {
+  it('should be truthy when `value` is a function', () => {
+    expect(isFunction(() => {})).toBeTruthy();
+    expect(() => {}).toBeTruthy();
+  });
+
+  it('should be falsy when `value` is not a function', () => {
+    expect(isFunction(null)).toBeFalsy();
+    expect(isFunction(undefined)).toBeFalsy();
+    expect(isFunction(NaN)).toBeFalsy();
+    expect(isFunction([])).toBeFalsy();
+    expect(isFunction('str')).toBeFalsy();
+    expect(isFunction(0)).toBeFalsy();
+  });
+});
+
+describe('isObject', () => {
+  it('should be truthy when `value` is a function', () => {
+    expect(isObject({})).toBeTruthy();
+  });
+
+  it('should be falsy when `value` is not a function', () => {
+    expect(isObject(() => {})).toBeFalsy();
+    expect(isObject(null)).toBeFalsy();
+    expect(isObject(undefined)).toBeFalsy();
+    expect(isObject(NaN)).toBeFalsy();
+    expect(isObject([])).toBeFalsy();
+    expect(isObject('str')).toBeFalsy();
+    expect(isObject(0)).toBeFalsy();
   });
 });
