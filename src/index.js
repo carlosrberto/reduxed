928c4cda38b53399716be43cd1d7c0c3b8f7da7c
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2019-01-12 12:22:26 -0200

    add flow types

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
+/* @flow */
 import { getActionType, isObject, isFunction } from './helpers';
 import { SCOPE_PROPERTY } from './constants';
 
-export const create = (...handlers) =>
-  (initialState, options = { typePrefix: '' }) => {
-    const handlersByType = handlers.reduce((acc, { name, fn }) => {
+type ActionHandler = (any, any) => any;
+
+type ReduxAction = { type: string, payload: any };
+
+type ReduxReducer = (any, ReduxAction) => any;
+
+type ReduxActionTypes = {
+  [string]: string
+};
+
+type ReduxActionCreators = { [string]: (any) => ReduxAction };
+
+type Handler = {
+  name: string,
+  fn: ActionHandler,
+};
+
+type Options = {
+  typePrefix: string,
+}
+
+type ReducerDescriptor = {
+  handlers: Array<Handler>,
+  handlersByType: { [string]: ActionHandler },
+  initialState: any,
+  options: Options,
+}
+
+export const create = (...handlers: Array<Handler>) =>
+  (initialState: any, options: Options = { typePrefix: '' }): ReducerDescriptor => {
+    const handlersByType: { [string]: ActionHandler } = handlers.reduce((acc, { name, fn }) => {
       acc[getActionType(options.typePrefix, name)] = fn;
       return acc;
     }, {});
@@ -12,10 +41,13 @@ export const create = (...handlers) =>
     });
   };
 
-export const handler = (name, fn) =>
+export const handler = (
+  name: string,
+  fn: ActionHandler,
+): Handler =>
   ({ name, fn });
 
-export const getReducer = ({ handlersByType, initialState }) =>
+export const getReducer = ({ handlersByType, initialState }: ReducerDescriptor): ReduxReducer =>
   (state, action) => {
     const nextState = state === undefined ? initialState : state;
     if (isObject(action) && isFunction(handlersByType[action.type])) {
@@ -24,13 +56,19 @@ export const getReducer = ({ handlersByType, initialState }) =>
     return nextState;
   };
 
-export const getTypes = ({ handlers, options: { typePrefix } }) =>
+export const getTypes = ({
+  handlers,
+  options: { typePrefix },
+}: ReducerDescriptor): ReduxActionTypes =>
   handlers.reduce((acc, { name }) => {
     acc[name] = getActionType(typePrefix, name);
     return acc;
   }, {});
 
-export const getActions = ({ handlers, options: { typePrefix } }) =>
+export const getActions = ({
+  handlers,
+  options: { typePrefix },
+}: ReducerDescriptor): ReduxActionCreators =>
   handlers.reduce((acc, { name }) => {
     acc[name] = payload => ({
       type: getActionType(typePrefix, name),
@@ -39,7 +77,10 @@ export const getActions = ({ handlers, options: { typePrefix } }) =>
     return acc;
   }, {});
 
-export const actionScope = (scope, actions) =>
+export const actionScope = (
+  scope: string,
+  actions: ReduxActionCreators,
+): ReduxActionCreators =>
   Object.keys(actions).reduce((acc, key) => {
     const actionCreator = actions[key];
     const fn = (...args) => {
@@ -53,22 +94,28 @@ export const actionScope = (scope, actions) =>
     return acc;
   }, {});
 
-export const reducerScope = (scope, reducer) => (state, action) => {
+export const reducerScope = (
+  scope: string,
+  reducer: ReduxReducer,
+): ReduxReducer => (state, action) => {
   if (action && action[SCOPE_PROPERTY] === scope) {
     return reducer(state, action);
   }
   return reducer(state, {});
 };
 
-export const withScope = (scope, target) => {
+export const withScope = (
+  scope: any,
+  target: any,
+) => {
   if (target !== undefined) {
     if (isFunction(target)) {
       return reducerScope(scope, target);
-    } if (isObject(target)) {
+    } else if (isObject(target)) {
       return actionScope(scope, target);
     }
     throw Error('Invalid scope target. It should be a reducer function or an object containing action creators');
   } else {
-    return arg => withScope(scope, arg);
+    return (arg: any) => withScope(scope, arg);
   }
 };
